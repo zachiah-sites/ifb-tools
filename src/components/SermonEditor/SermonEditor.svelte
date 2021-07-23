038955e5574147466cf8114604f62e43b971295f
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2021-07-22 17:23:12 -0700

    prevent sermon content from showing before loading the editor Fixes #19

--- src/components/SermonEditor/SermonEditor.svelte
+++ src/components/SermonEditor/SermonEditor.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './sermon-editor.css';
-	import { createEventDispatcher, onMount } from 'svelte';
+	import { createEventDispatcher, onMount, tick } from 'svelte';
 	//import { Autosave } from '@ckeditor/ckeditor5-autosave';
 
 	export let initialData: string;
@@ -10,10 +10,13 @@
 
 	let toolbarContainer;
 	let editable;
+	let showContent = false;
 	onMount(async () => {
 		const DecoupledEditor = (
 			await import('@ckeditor/ckeditor5-build-decoupled-document/build/ckeditor.js')
 		).default;
+		showContent = true;
+		await tick();
 		editor = await DecoupledEditor.create(editable, {
 			cloudServices: {}
 		});
@@ -26,8 +29,10 @@
 	<div class="document-editor__toolbar" bind:this={toolbarContainer} />
 	<div class="document-editor__editable-container">
 		<div class="document-editor__editable" bind:this={editable}>
-			{@html initialData ||
-				`<ol style="list-style-type:decimal;"><li>Intro<ol style="list-style-type:upper-latin;"><li>Title</li><li>Verse</li><li>Pray</li></ol></li><li>Point 1<ol style="list-style-type:upper-latin;"><li>Subpoint 1</li><li>Subpoint 2</li></ol></li><li>Conclusion</li></ol>`}
+			{#if showContent}
+				{@html initialData ||
+					`<ol style="list-style-type:decimal;"><li>Intro<ol style="list-style-type:upper-latin;"><li>Title</li><li>Verse</li><li>Pray</li></ol></li><li>Point 1<ol style="list-style-type:upper-latin;"><li>Subpoint 1</li><li>Subpoint 2</li></ol></li><li>Conclusion</li></ol>`}
+			{/if}
 		</div>
 	</div>
 </div>
