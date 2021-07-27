fb098572ac840517c892f8c19aefa521fdfc8bf9
AlexBall03 <alexanderdball03@gmail.com>
2021-07-27 16:42:14 -0700

    Added fade to nav component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>IFB Tools</h1>
+
+<p class="bg-teal">IFB Tools is a placa for Independent Fundemental Baptists(IFB) to serve the Lord on the go through the internet.
+Here you can read/memorize the Bible(KJV), watch Biblical preaching and more. If you havn't already, sign up today for the best experience!</p>
