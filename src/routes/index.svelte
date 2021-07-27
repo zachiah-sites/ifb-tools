44120d9d01b2d5b2edfd028b0b919b7f4f9ae354
AlexBall03 <alexanderdball03@gmail.com>
2021-07-27 16:56:10 -0700

    Reset index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,3 @@
-<h1>IFB Tools</h1>
+<h1>Welcome to SvelteKit</h1>
 
-<p class="bg-teal">IFB Tools is a placa for Independent Fundemental Baptists(IFB) to serve the Lord on the go through the internet.
-Here you can read/memorize the Bible(KJV), watch Biblical preaching and more. If you havn't already, sign up today for the best experience!</p>
+<p class="bg-teal">Visit kit.svelte.dev to read the documentation</p>
