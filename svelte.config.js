fa03616d6ef4475210ec2a752c12a699f468c99e
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2021-07-29 08:45:22 -0700

    ditch netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import path from 'path';
-import netlify from '@sveltejs/adapter-netlify';
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -23,7 +23,7 @@ const config = {
 		},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: netlify()
+		adapter: node()
 	}
 };
 
