f81180ed4be13e4f3fce5d570cc9437fac20a122
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2021-08-07 12:45:32 -0700

    fix puncuation on home page Fixes #53

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,20 +4,20 @@
 
 <Jumbotron title="IFB Tools (alpha)">
 	<p class="text-lg mb-2">Welcome to IFB Tools</p>
-	<p class="mb-4">IFB Tools aims to provide useful tools for Independent Fundemental Baptists</p>
+	<p class="mb-4">IFB Tools aims to provide useful tools for Independent Fundemental.</p>
 	<p class="mb-4">
-		Please note that IFB Tools is in VERY early development so lots of things are incomplete, or
+		Please note that IFB Tools is in VERY early development, so lots of things are incomplete or
 		buggy. For an example just click the map icon on the bottom.
 	</p>
 	<p class="mb-4">
-		If you find any issues relating to the sermon editor or Bible that I don't have yet please
+		If you find any issues relating to the sermon editor or Bible that I don't have yet, please
 		report them on <a href="https://github.com/zachiah-sites/ifb-tools"
 			>this site's github repository</a
 		>.
 	</p>
 	<p class="mb-4">
-		Contributors are welcome if you would like to help out with IFB tools you can get started on one
-		of the exsisting issues and create a pull request.
+		Contributors are welcome. If you would like to help out with IFB tools, you can get, started on
+		one of the existing issues and create a pull request.
 	</p>
 	<p>Thanks!</p></Jumbotron
 >
